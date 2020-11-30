0add89e5c7b6421afd0a49c30922d3da73ed6c5c
Bayapa Reddy <bayapareddy.putlur8@gmail.com>
2020-11-30 17:46:47 +0530

    Add files via upload

---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+
+// Set variables for our request
+$shop = $_GET['shop'];
+$api_key = "8a318358c219477fd312d451974781b9";
+$scopes = "read_orders,write_products";
+$redirect_uri = "http://apptesting.22web.org/apps/generate_token.php";
+
+// Build install/approval URL to redirect to
+$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+
+// Redirect
+header("Location: " . $install_url);
+die();
