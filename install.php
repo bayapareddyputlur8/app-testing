510d413dca7bd14301f18bb195cf1852ac4f9787
Bayapa Reddy <bayapareddy.putlur8@gmail.com>
2020-11-30 19:02:27 +0530

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "8a318358c219477fd312d451974781b9";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://apptesting.22web.org/apps/generate_token.php";
+$redirect_uri = "https://frndz-test.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
